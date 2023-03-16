be1a169bc80bf95ddd5f22120cca27c0fce2b439
mahmoud choukari <mahmoud.choukari@gmail.com>
2023-03-16 14:05:48 -0700

    c basics

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - A programm that print a line with a put function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
