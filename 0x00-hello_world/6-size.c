e30eddb0153cf07cd2e449e568d980ce0e68ea39
mahmoud choukari <mahmoud.choukari@gmail.com>
2023-03-16 16:30:09 -0700

    start programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-printf("size of char: %zu byte(s)\n", sizeof(char));
-printf("size of int: %zu byte(s)\n", sizeof(int));
-printf("size of long int: %zu byte(s)\n", sizeof(long int));
-printf("size of long long int: %zu byte(s)\n", sizeof(long long int));
-printf("size of float: %zu byte(s)\n", sizeof(float));
+printf("size of char: %lu byte(s)", sizeof(char));
+printf("size of int: %lu byte(s)", sizeof(int));
+printf("size of long int: %lu byte(s)", sizeof(long int));
+printf("size of long long int: %lu byte(s)", sizeof(long long int));
+printf("size of float: %lu byte(s)", sizeof(float));
 return (0);
 }
