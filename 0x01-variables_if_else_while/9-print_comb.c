002e1b0e8f63c27c4c588d617fac5ea04c8625e1
mahmoud choukari <mahmoud.choukari@gmail.com>
2023-03-19 09:51:21 -0700

    else if wihle and for

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+
+	/* your code goes there */
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+
+	return (0);
+}
