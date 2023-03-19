4fc904593dcddc2a997ede0fdb18430752275f7a
mahmoud choukari <mahmoud.choukari@gmail.com>
2023-03-19 09:28:52 -0700

    else if wihle and for

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+
+	/* your code goes there */
+	char i;
+
+	for (i = 'z'; i >= 'a' ; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
