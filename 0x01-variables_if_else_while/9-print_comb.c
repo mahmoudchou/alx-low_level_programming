941f47a4e3eaadfbebdd20b336d2060da3b49fc8
mahmoud choukari <mahmoud.choukari@gmail.com>
2023-03-19 09:56:38 -0700

    else if wihle and for

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -18,8 +18,11 @@ int main(void)
 	for (i = 0 ; i <= 9 ; i++)
 	{
 		putchar(i + '0');
-		putchar(',');
-		putchar(' ');
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}	
 	}
 
 	return (0);
