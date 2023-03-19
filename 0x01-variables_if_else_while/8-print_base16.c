81f078a75aa96ef1fbc136f9dfaf72b7ee93bccc
mahmoud choukari <mahmoud.choukari@gmail.com>
2023-03-19 09:37:16 -0700

    else if wihle and for

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+
+	/* your code goes there */
+	int i;
+	char j;
+
+	for (i = 0 ; i <= 9 ; i++)
+		putchar(i + '0');
+
+	for (j = 'a' ; j <= 'f' ; j++)
+		putchar (j);
+
+	putchar('\n');
+	return (0);
+}
